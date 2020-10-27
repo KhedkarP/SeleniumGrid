1ce64bfb4b3ece36cbfcbe6b29d2b5ef3874b7e3
pkhedkar <pkhedkar@host.docker.internal>
2020-10-27 17:49:10 +0530

    Parallel execution achieved via maven

--- Grid/src/test/java/SeleniumGrid/Grid/SearchGoogle.java
+++ Grid/src/test/java/SeleniumGrid/Grid/SearchGoogle.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class GoogleSearch {
+public class SearchGoogle {
 	
 	
 @Test
@@ -23,7 +23,7 @@ public void Googlesearch() throws MalformedURLException {
 	driver.findElement(By.id("result-stats")).isDisplayed();
 	driver.findElement(By.xpath("//a[@title='Go to Google Home']")).click();
 	String Title = driver.getTitle();
-	System.out.println("Title Is : Suite 1 :TC1 : "+Title);
+	System.out.println("Title Is : Suite2 : TC1 : "+Title);
 	driver.quit();
 	
 	}
@@ -38,33 +38,8 @@ public void Googlesearch1() throws MalformedURLException {
 	driver.findElement(By.id("result-stats")).isDisplayed();
 	driver.findElement(By.xpath("//a[@title='Go to Google Home']")).click();
 	String Title = driver.getTitle();
-	System.out.println("Title Is : Suite 1 : TC2 : "+Title);
+	System.out.println("Title Is : Suite2 : TC2 : "+Title);
 	driver.quit();
 	}
 
-@Test
-public void Googlesearch2() throws MalformedURLException {
-	DesiredCapabilities capabilities = new DesiredCapabilities();
-	capabilities.setBrowserName("chrome");
-	WebDriver driver= new RemoteWebDriver(new URL("http://192.168.43.76:4444/wd/hub"),capabilities);
-	driver.get("https://www.google.com/");
-	driver.findElement(By.name("q")).sendKeys("Selenium Hub" + Keys.ENTER);
-	String Title = driver.getTitle();
-	System.out.println("Title Is : Suite 1 : TC3 : "+Title);
-	driver.quit();
-	
-	}/*
-@Test
-public void Googlesearch3() throws MalformedURLException {
-	DesiredCapabilities capabilities = new DesiredCapabilities();
-	capabilities.setBrowserName("firefox");
-	capabilities.setCapability("marionette", true);
-	WebDriver driver= new RemoteWebDriver(new URL("http://192.168.43.76:4444/wd/hub"),capabilities);
-	driver.get("https://www.google.com/");
-	driver.findElement(By.name("q")).sendKeys("Selenium Hub" + Keys.ENTER);
-	String Title = driver.getTitle();
-	System.out.println("Title Is : Suite 1 : TC4 : "+Title);
-	driver.quit();
-	
-	}*/
 }
